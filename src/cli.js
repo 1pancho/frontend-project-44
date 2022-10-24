c50c81268c86aaaa4a078951f7611b7a5e1ba316
Timerlan Medov <medovcloud@me.com>
2022-10-24 15:14:59 +0400

    Third step

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greetingUser = () => {
+ const userName = readlineSync.question('May I have your name? ');
+ return(`Hello, ${userName}!`);
+};
+
+export default greetingUser;
