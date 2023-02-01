1d507bb83774fefca72c01a15002d953a7676f67
Timerlan Medov <medovcloud@me.com>
2023-02-01 07:07:21 +0700

    new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const gameLogic = (rule, dataGenerationFunction) => {
     console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name?');
+    const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}`);
     console.log(rule);
     const numberOfRounds = 3;
